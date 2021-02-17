42aefc980b0d0f6da87f982382d6c1690681ae2b
moh2006-py <74707334+moh2006-py@users.noreply.github.com>
2021-02-17 16:27:44 +0300

    Add files via upload

---
+++ sendemail.php
@@ -0,0 +1,43 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+
+require_once 'phpmailer/Exception.php';
+require_once 'phpmailer/PHPMailer.php';
+require_once 'phpmailer/SMTP.php';
+
+$mail = new PHPMailer();
+
+$alert = '';
+
+if(isset($_POST['submit'])){
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+
+  try{
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'hmodymtz@gmail.com'; // Gmail address which you want to use as SMTP server
+    $mail->Password = 'mohkill@ru'; // Gmail address Password
+    $mail->SMTPSecure = "ssl";
+    $mail->Port = '465';
+
+    $mail->setFrom('hmodymtz@gmail.com'); // Gmail address which you used as SMTP server
+    $mail->addAddress('hmodymtz@gmail.com'); // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
+
+    $mail->isHTML(true);
+    $mail->Subject = 'Message Received (Contact Page)';
+    $mail->Body = "<h3>Name : $name <br>Email: $email <br>Message : $message</h3>";
+
+    $mail->send();
+    $alert = '<div class="alert-success">
+                 <span>Message Sent! Thank you for contacting us.</span>
+                </div>';
+  } catch (Exception $e){
+    $alert = '<div class="alert-error">
+                <span>'.$e->getMessage().'</span>
+              </div>';
+  }
+}
+?>
